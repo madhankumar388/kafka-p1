826529c0572d8a85ae2504368ed5f1475f8ae496
Madhan kumar <madhankumar388@gmail.com>
2021-07-10 19:40:26 -0500

    yes

--- db_backup/restoredb.sh
+++ db_backup/restoredb.sh
@@ -1,4 +1,5 @@
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password1# -Q"RESTORE DATABASE TestDB
+#!/bin/bash
+sleep 25 && /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password1# -Q"RESTORE DATABASE TestDB
 FROM DISK = '/db_backup/TestDB-2021627-16-47-11.bak'
 WITH REPLACE
-GO"
+GO" & /opt/mssql/bin/sqlservr
